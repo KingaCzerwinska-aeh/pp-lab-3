4827c84cf481c6260d28b0c6ac76cca1be1a7e1d
KingaCzerwinska-aeh <162008074+KingaCzerwinska-aeh@users.noreply.github.com>
2024-04-06 19:20:53 +0200

    Zadanie 5

---
+++ Main.java
@@ -0,0 +1,19 @@
+import geometry.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle[] circles = new Circle[2];
+        circles[0] = new Circle(new Point(0, 0), 5);
+        circles[1] = new ColoredCircle(new Point(5, 20), 9, "red");
+
+        for (Circle circle : circles) {
+            System.out.println("The area of the circle [radius = " + circle.getRadius() + "]: ");
+            System.out.println("Area: " + circle.calculateArea());
+
+            if (circle instanceof ColoredCircle) {
+                String color = ((ColoredCircle) circle).getColor();
+                System.out.println("Color: " + color);
+            }
+        }
+    }
+}
